3cacbf5752a6a01a283a1a7dd06508c261234230
Vladyslav Kukharchuk <vladyslavziuziun@gmail.com>
2022-10-29 00:57:51 +0300

    Finished work on task 2

--- lecture-4/Task2.js
+++ lecture-4/Task2.js
@@ -7,25 +7,23 @@
 // double(5);  // => 10
 // square(5); // => 25
 
-function multiply(num1, num2 = 2) {
+function multiply(num1, num2) {
     return num1 * num2;
 }
 
 multiply(2, 6);
 console.log(`Result of the function multiply: ${multiply(2, 6)}`);
 
-// function double(num){
-//     return multiply(num);
-// }
-
-let double = (num) => multiply(num);;
-
-double(5);
-console.log(`Result of the function double: ${double(5)}`);
-
-function square(num){
-    return num ** 2;
+function double(num) {
+    return multiply(num, 2);
 }
 
-square(5);
+double(5); // => 10
+console.log(`Result of the function double: ${double(5)}`);
+
+function square(num) {
+    return multiply(num, num);
+}
+
+square(5); // => 25
 console.log(`Result of the function square: ${square(5)}`);
