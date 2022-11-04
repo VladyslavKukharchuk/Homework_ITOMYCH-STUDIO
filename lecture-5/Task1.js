6624c78d2ceec0cd60b06f234ce52d74fc7b651d
Vladyslav Kukharchuk <vladyslavziuziun@gmail.com>
2022-11-04 22:06:02 +0200

    In process

--- lecture-5/Task1.js
+++ lecture-5/Task1.js
@@ -5,3 +5,58 @@
 // - Знаходить перетин об'єктів і повертає об'єкт перетинів.
 // - Знаходить значення за заданим ключем
 
+const firstObject = {
+    "name": "Ruslan"
+}
+
+const secondObject = {};
+
+const emptyObject = {};
+console.log(emptyObject);
+
+function objectIsEmpty(object) {
+    console.log(object);
+    if(object == emptyObject){
+        console.log(true);
+    }else {
+        console.log(false);
+    }
+}
+
+function comparisonOfObjects(firstObject, secondObject) {
+
+}
+
+function fingCrossingOfObjects(firstObject, secondObject) {
+
+}
+
+function findValueByKey(key) {
+    // console.log(firstObject.key);
+    // console.log(firstObject['key']);
+    // console.log(firstObject["key"]);
+    // console.log(firstObject[`key`]);
+    // console.log(firstObject.name);
+    // let value = firstObject.key;
+    // if(value === undefined){
+    //     return `No values found by key: "${key}".`
+    // } else {
+    //     return `By key "${key}" was found: "${value}".`
+    // }
+
+    if(key in firstObject){
+        return `Існує значення з ключом "${key}".`
+    } else {
+        return `Значення з ключом "${key}" не існує.`
+    }
+}
+
+console.log(`---objectIsEmpty---`);
+
+objectIsEmpty(firstObject);
+objectIsEmpty(secondObject);
+
+console.log(`---findValueByKey---`);
+
+console.log(findValueByKey("name"));
+
--- lecture-5/Task2.js
+++ lecture-5/Task2.js
@@ -4,3 +4,27 @@
 
 // Також фукцію пошуку площі перетину двох кіл.
 
+const Circle = {
+    x: 1,
+    y: 2,
+    r: 201,
+
+    getAreaOfCircle(){
+        let areaOfCircle = Math.PI * Math.pow(this.r, 2);
+        return areaOfCircle;
+    },
+
+    getPerimeterOfCircle(){
+        let perimeterOfCircle = 2 * Math.PI * this.r;
+        return perimeterOfCircle;
+    }
+}
+
+function getAreaOfIntersectionOfCircles(firstСircle, secondCircle){
+    // intersectionChecking 
+
+    
+}
+
+console.log(`The area of the circle is: ${Circle.getAreaOfCircle().toFixed(1)}.`);
+console.log(`The perimeter of the circle is: ${Circle.getPerimeterOfCircle().toFixed(1)}.`);
