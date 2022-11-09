2bb80dca988df2dfea80ca469efed5b9e2ecb416
Vladyslav Kukharchuk <vladyslavziuziun@gmail.com>
2022-11-09 16:34:56 +0200

    Finished work on task 2

--- lecture-6/Task2.js
+++ lecture-6/Task2.js
@@ -1,3 +1,21 @@
 // Написати метод для виведення унікальних значень в масиві.
 // [1, 2, 2, 3, 3, 3] => [1, 2, 3]
 
+function getUniqueElements(arr) {
+    //simple solution
+    let result = [];
+    for (let item of arr) {
+        if (!result.includes(item)) {
+            result.push(item);
+        }
+    }
+    return result;
+
+    //solution dased on Set
+    // return Array.from(new Set(arr));
+}
+
+let arr = [1, 2, 2, 3, 3, 3];
+
+console.log(arr);
+console.log(getUniqueElements(arr));
