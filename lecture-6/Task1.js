e0724007da2787cce46ad760cf6b21df78b6d4ad
Vladyslav Kukharchuk <vladyslavziuziun@gmail.com>
2022-11-10 01:05:47 +0200

    Finished work on task 1

--- lecture-6/Task1.js
+++ lecture-6/Task1.js
@@ -1,58 +1,63 @@
 // Нехай задано дві прямокутні матриці A та B розмірності m × n та n × q відповідно:
 // Знайти матрицю C.
 
-let A = [
+let matrixA = [
     [1, 2],
     [3, 4]
 ];
 
-let B = [
+let matrixB = [
     [1, 2],
     [4, 5]
 ];
 
-let C = [
+let matrixC = [
     [1, 2],
     [4, 5],
     [1, 2]
 ];
 
-let D = [
+let matrixD = [
     [1, 2, 5],
     [4, 5, 2]
 ];
 
-function multiplyMatrices(A, B) {
+function multiply(A, B) {
     let rowsA = A.length;
     let colsA = A[0].length;
     let rowsB = B.length;
     let colsB = B[0].length;
-    let C = [];
-    if (colsA != rowsB) return false;
-    for (let q = 0; q < rowsA; q++) C[q] = [];
-    for (let m = 0; m < colsB; m++) {
-        for (let i = 0; i < rowsA; i++) {
-            let c = 0;
-            for (let n = 0; n < rowsB; n++) {
-                c += A[i][n] * B[n][m]
-            };
-            C[i][m] = c;
+    if (colsA !== rowsB) {
+        return false
+    } else {
+        let C = [];
+        for (let q = 0; q < rowsA; q++) {
+            C[q] = []
+        };
+        for (let m = 0; m < colsB; m++) {
+            for (let i = 0; i < rowsA; i++) {
+                let c = 0;
+                for (let n = 0; n < rowsB; n++) {
+                    c += A[i][n] * B[n][m]
+                };
+                C[i][m] = c;
+            }
         }
-    }
-    return C;
+        return C;
+    };
 }
 
-console.log(multiplyMatrices(A, B));
-console.log(multiplyMatrices(A, B) ? `The result of matrix multiplication: ${multiplyMatrices(A, B)}` : "The number of columns of the first matrix must be equal to the number of rows of the second.");
+console.log(multiply(matrixA, matrixB));
+console.log(multiply(matrixA, matrixB) ? `The result of matrix multiplication: ${multiply(matrixA, matrixB)}` : "The number of columns of the first matrix must be equal to the number of rows of the second.");
 
-console.log(multiplyMatrices(C, B));
-console.log(multiplyMatrices(C, B) ? `The result of matrix multiplication: ${multiplyMatrices(C, B)}` : "The number of columns of the first matrix must be equal to the number of rows of the second.");
+console.log(multiply(matrixC, matrixB));
+console.log(multiply(matrixC, matrixB) ? `The result of matrix multiplication: ${multiply(matrixC, matrixB)}` : "The number of columns of the first matrix must be equal to the number of rows of the second.");
 
-console.log(multiplyMatrices(B, C));
-console.log(multiplyMatrices(B, C) ? `The result of matrix multiplication: ${multiplyMatrices(B, C)}` : "The number of columns of the first matrix must be equal to the number of rows of the second.");
+console.log(multiply(matrixB, matrixC));
+console.log(multiply(matrixB, matrixC) ? `The result of matrix multiplication: ${multiply(matrixB, matrixC)}` : "The number of columns of the first matrix must be equal to the number of rows of the second.");
 
-console.log(multiplyMatrices(A, D));
-console.log(multiplyMatrices(A, D) ? `The result of matrix multiplication: ${multiplyMatrices(A, D)}` : "The number of columns of the first matrix must be equal to the number of rows of the second.");
+console.log(multiply(matrixA, matrixD));
+console.log(multiply(matrixA, matrixD) ? `The result of matrix multiplication: ${multiply(matrixA, matrixD)}` : "The number of columns of the first matrix must be equal to the number of rows of the second.");
 
-console.log(multiplyMatrices(D, A));
-console.log(multiplyMatrices(D, A) ? `The result of matrix multiplication: ${multiplyMatrices(D, A)}` : "The number of columns of the first matrix must be equal to the number of rows of the second.");
+console.log(multiply(matrixD, matrixA));
+console.log(multiply(matrixD, matrixA) ? `The result of matrix multiplication: ${multiply(matrixD, matrixA)}` : "The number of columns of the first matrix must be equal to the number of rows of the second.");
