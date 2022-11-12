9cdf3224e9b13d3e95cc656b23d5d7f2263e96db
Vladyslav Kukharchuk <vladyslavziuziun@gmail.com>
2022-11-12 03:21:53 +0200

    The method for calculating area of intersection of rectangles is finished

--- lecture-7/Task1.js
+++ lecture-7/Task1.js
@@ -3,23 +3,30 @@
 // ● 5 різних класів (Геометричні фігури)
 // ● 5 унікальних методів
 
-// class Square {
-//     constructor(x1, y1, x2, y2) {
-//         this.p1.x = x1;
-//         this.p1.y = y1;
-//         this.p2.x = x2;
-//         this.p2.y = y2;
-//     }
-// }
+class Square {
+    
+    #p1;
+    #p2;
 
-// const firstSquare = new Square('Jhon', 'See');
+    constructor(x1, y1, x2, y2) {
+        this.p1 = {
+            x: x1,
+            y: y1
+        };
+        this.p2 = {
+            x: x2,
+            y: y2
+        };
+    }
+}
+
+//Додатні координати
+const firstSquare = new Square(0, 0, 100, 100);
+//Пересікаються 1 кутом
+const secondSquare = new Square(50, 50, 150, 150);
 
 class Rectangle {
     constructor(x1, y1, x2, y2) {
-        // this.p1.x = x1;
-        // this.p1.y = y1;
-        // this.p2.x = x2;
-        // this.p2.y = y2;
         this.p1 = {
             x: x1,
             y: y1
@@ -30,78 +37,79 @@ class Rectangle {
         };
     }
 
-    getAreaOfIntersectionOfRectangles(rectangle) {
+    getAreaOfIntersection(rectangle) {
         let firstRectangle = this;
         let secondRectangle = rectangle;
 
-        // console.log(firstRectangle);
-        // console.log(secondRectangle);
-        // console.log(firstRectangle.p1.x);
-        // console.log(firstRectangle.p1.y);
-        // console.log(firstRectangle.p2.x);
-        // console.log(firstRectangle.p2.y);
-
         let left = Math.max(firstRectangle.p1.x, secondRectangle.p1.x);
-        console.log(`left: ${left}`);
         let top = Math.min(firstRectangle.p2.y, secondRectangle.p2.y);
-        console.log(`top: ${top}`);
         let right = Math.min(firstRectangle.p2.x, secondRectangle.p2.x);
-        console.log(`right: ${right}`);
         let bottom = Math.max(firstRectangle.p1.y, secondRectangle.p1.y);
-        console.log(`bottom: ${bottom}`);
         let witdth = right - left;
-        console.log(`witdth: ${witdth}`);
         let height = top - bottom;
-        console.log(`height: ${height}`);
         if ((witdth <= 0) || (height <= 0)) {
             return false;
         } else {
-            console.log(`return: ${witdth * height}`);
             return witdth * height;
         }
     }
 }
 
-// const firstRectangle = new Rectangle(0, 0, 2, 3);
-// const secondRectangle = new Rectangle(1, 1, 3, 4);
+//Додатні координати
+const Rectangle1 = new Rectangle(100, 0, 400, 200);
+//Пересікаються 1 кутом
+const rectangle2 = new Rectangle(200, 100, 500, 300);
+//Пересікаються двома кутами
+const rectangle3 = new Rectangle(0, 50, 200, 150);
+//Проходить через середину
+const rectangle4 = new Rectangle(0, 50, 500, 150);
+//Пересікаються двома кутами інший бік
+const rectangle5 = new Rectangle(300, 50, 500, 150);
+//Повністю пересікаються
+const rectangle6 = new Rectangle(150, 50, 350, 150);
+//Не пересікаються
+const rectangle7 = new Rectangle(100, 300, 400, 500);
 
-const firstRectangle = new Rectangle(100, 0, 400, 200);
-const secondRectangle = new Rectangle(200, 100, 500, 300);
-
-const thirdRectangle = new Rectangle(0, 50, 200, 150);
-
-const fourthRectangle = new Rectangle(0, 50, 500, 150);
-
-const fifthRectangle = new Rectangle(300, 50, 500, 150);
-
-const sixthRectangle = new Rectangle(150, 50, 350, 150);
-
-const seventhRectangle = new Rectangle(100, 300, 400, 500);
-
-// const eightRectangle = new Rectangle(, , , );
-
-// const ninthRectangle = new Rectangle(, , , );
-// const tenthRectangle = new Rectangle(, , , );
+//Відємні координатами
+const rectangle8 = new Rectangle(-400, -200, -100, 0);
+//Пересікаються 1 кутом
+const rectangle9 = new Rectangle(-500, -300, -200, -100);
+//Пересікаються двома кутами
+const rectangle10 = new Rectangle(-200, -150, 0, -50);
+//Проходить через середину
+const rectangle11 = new Rectangle(-500, -150, 0, -50);
+//Пересікаються двома кутами інший бік
+const rectangle12 = new Rectangle(-500, -150, -300, -50);
+//Повністю пересікаються
+const rectangle13 = new Rectangle(-350, -150, -150, -50);
+//Не пересікаються
+const rectangle14 = new Rectangle(-400, -500, -100, -300);
 
 //Пересікаються 1 кутом
-console.log(secondRectangle.getAreaOfIntersectionOfRectangles(firstRectangle) ? `Area of intersection of rectangles: ${secondRectangle.getAreaOfIntersectionOfRectangles(firstRectangle).toFixed(2)}` : `The rectangles do not intersect.`);
-
+console.log(rectangle2.getAreaOfIntersection(Rectangle1) ? `Area of intersection of rectangles: ${rectangle2.getAreaOfIntersection(Rectangle1)}` : `The rectangles do not intersect.`);
 //Пересікаються двома кутами
-console.log(thirdRectangle.getAreaOfIntersectionOfRectangles(firstRectangle) ? `Area of intersection of rectangles: ${thirdRectangle.getAreaOfIntersectionOfRectangles(firstRectangle).toFixed(2)}` : `The rectangles do not intersect.`);
-
+console.log(rectangle3.getAreaOfIntersection(Rectangle1) ? `Area of intersection of rectangles: ${rectangle3.getAreaOfIntersection(Rectangle1)}` : `The rectangles do not intersect.`);
 //Проходить через середину
-console.log(fourthRectangle.getAreaOfIntersectionOfRectangles(firstRectangle) ? `Area of intersection of rectangles: ${fourthRectangle.getAreaOfIntersectionOfRectangles(firstRectangle).toFixed(2)}` : `The rectangles do not intersect.`);
-
+console.log(rectangle4.getAreaOfIntersection(Rectangle1) ? `Area of intersection of rectangles: ${rectangle4.getAreaOfIntersection(Rectangle1)}` : `The rectangles do not intersect.`);
 //Пересікаються двома кутами інший бік
-console.log(fifthRectangle.getAreaOfIntersectionOfRectangles(firstRectangle) ? `Area of intersection of rectangles: ${fifthRectangle.getAreaOfIntersectionOfRectangles(firstRectangle).toFixed(2)}` : `The rectangles do not intersect.`);
-
+console.log(rectangle5.getAreaOfIntersection(Rectangle1) ? `Area of intersection of rectangles: ${rectangle5.getAreaOfIntersection(Rectangle1)}` : `The rectangles do not intersect.`);
 //Повністю пересікаються
-console.log(sixthRectangle.getAreaOfIntersectionOfRectangles(firstRectangle) ? `Area of intersection of rectangles: ${sixthRectangle.getAreaOfIntersectionOfRectangles(firstRectangle).toFixed(2)}` : `The rectangles do not intersect.`);
-
+console.log(rectangle6.getAreaOfIntersection(Rectangle1) ? `Area of intersection of rectangles: ${rectangle6.getAreaOfIntersection(Rectangle1)}` : `The rectangles do not intersect.`);
 //Не пересікаються
-console.log(seventhRectangle.getAreaOfIntersectionOfRectangles(firstRectangle) ? `Area of intersection of rectangles: ${seventhRectangle.getAreaOfIntersectionOfRectangles(firstRectangle).toFixed(2)}` : `The rectangles do not intersect.`);
+console.log(rectangle7.getAreaOfIntersection(Rectangle1) ? `Area of intersection of rectangles: ${rectangle7.getAreaOfIntersection(Rectangle1)}` : `The rectangles do not intersect.`);
 
-
+//Пересікаються 1 кутом
+console.log(rectangle9.getAreaOfIntersection(rectangle8) ? `Area of intersection of rectangles: ${rectangle9.getAreaOfIntersection(rectangle8)}` : `The rectangles do not intersect.`);
+//Пересікаються двома кутами
+console.log(rectangle10.getAreaOfIntersection(rectangle8) ? `Area of intersection of rectangles: ${rectangle10.getAreaOfIntersection(rectangle8)}` : `The rectangles do not intersect.`);
+//Проходить через середину
+console.log(rectangle11.getAreaOfIntersection(rectangle8) ? `Area of intersection of rectangles: ${rectangle11.getAreaOfIntersection(rectangle8)}` : `The rectangles do not intersect.`);
+//Пересікаються двома кутами інший бік
+console.log(rectangle12.getAreaOfIntersection(rectangle8) ? `Area of intersection of rectangles: ${rectangle12.getAreaOfIntersection(rectangle8)}` : `The rectangles do not intersect.`);
+//Повністю пересікаються
+console.log(rectangle13.getAreaOfIntersection(rectangle8) ? `Area of intersection of rectangles: ${rectangle13.getAreaOfIntersection(rectangle8)}` : `The rectangles do not intersect.`);
+//Не пересікаються
+console.log(rectangle14.getAreaOfIntersection(rectangle8) ? `Area of intersection of rectangles: ${rectangle14.getAreaOfIntersection(rectangle8)}` : `The rectangles do not intersect.`);
 
 
 // class User {
@@ -129,7 +137,7 @@ class Circle {
         return areaOfCircle;
     };
 
-    getAreaOfIntersectionOfCircles(circle) {
+    getAreaOfIntersection(circle) {
         let firstСircle = this;
         let secondCircle = circle;
 
@@ -156,7 +164,7 @@ class Circle {
 const firstСircle = new Circle(0, 0, 3);
 const secondCircle = new Circle(5, 0, 4);
 
-console.log(`Area of intersection of circles: ${firstСircle.getAreaOfIntersectionOfCircles(secondCircle).toFixed(2)}`);
+console.log(`Area of intersection of circles: ${firstСircle.getAreaOfIntersection(secondCircle).toFixed(2)}`);
 
 // Home task 2
 // Для заданих фігур написати метод який обраховує площу перетину заданих фігур.
