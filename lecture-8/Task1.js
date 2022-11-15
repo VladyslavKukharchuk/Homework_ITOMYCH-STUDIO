d86b7b13a3fdeb8b0b982b2c0a0ab5b438d2a2c3
Vladyslav Kukharchuk <vladyslavziuziun@gmail.com>
2022-11-15 18:27:34 +0200

    Finished work on task 1

--- lecture-8/Task1.js
+++ lecture-8/Task1.js
@@ -8,52 +8,6 @@
 // ● Користувач може грати доки сам не захоче
 
 
-
-// readline = require("readline").createInterface({
-//     input: process.stdin,
-//     output: process.stdout,
-// });
-
-// function getRandomNumber(min, max) {
-//     return Math.floor(Math.random() * (max - min + 1)) + min;
-// }
-
-// let userPoints = 0;
-
-// (function loop() {
-//     readline.question("Enter a number between 1 and 6: ", (answer) => {
-
-//         let userNumber = +answer;
-//         let systemNumber = getRandomNumber(1, 6);
-//         let prize = 0;
-
-//         console.log(`You entered ${userNumber}, the system selected a ${systemNumber}.`);
-
-//         if (userNumber === systemNumber) {
-//             prize = 2;
-//             console.log(`Congratulations!!!! , you guessed the number.`);
-//         } else if ((userNumber === systemNumber) || (userNumber === systemNumber - 1) || (userNumber === systemNumber + 1)) {
-//             prize = 1;
-//             console.log(`You were close, keep the motivating prize: +1 point.`)
-//         } else {
-//             prize = 0;
-//             console.log(`This time you didn't guess, try again.`);
-//         }
-//         userPoints += prize;
-
-//         console.log(`You have ${userPoints} points.`);
-
-//         if (answer) {
-//             loop();
-//         }
-//     });
-// })();
-
-// readline.on('SIGINT', () => {
-//     console.log(`\nYou finished the game with a score of: ${userPoints} points.`);
-//     readline.close();
-// });
-
 readline = require("readline").createInterface({
     input: process.stdin,
     output: process.stdout,
@@ -65,28 +19,36 @@ function getRandomNumber(min, max) {
 
 let userPoints = 0;
 
+console.log(`\n  Welcome to game "Guess the number"!!!\n\n\n     Enter numbers and get points.\n\n      press <Ctrl + C> to exit.\n`);
+
+
 function loop() {
     readline.question("Enter a number between 1 and 6: ", (answer) => {
 
-        let userNumber = +answer;
-        let systemNumber = getRandomNumber(1, 6);
-        let prize = 0;
+        let userNumber = parseInt(answer);
 
-        console.log(`You entered ${userNumber}, the system selected a ${systemNumber}.`);
+        if (userNumber >= 1 && userNumber <= 6) {
+            let systemNumber = getRandomNumber(1, 6);
+            let prize = 0;
 
-        if (userNumber === systemNumber) {
-            prize = 2;
-            console.log(`Congratulations!!!! , you guessed the number.`);
-        } else if ((userNumber === systemNumber) || (userNumber === systemNumber - 1) || (userNumber === systemNumber + 1)) {
-            prize = 1;
-            console.log(`You were close, keep the motivating prize: +1 point.`)
+            console.log(`  You entered ${userNumber}, the system selected a ${systemNumber}.`);
+
+            if (userNumber === systemNumber) {
+                prize = 2;
+                console.log(`  Congratulations!!!! , you guessed the number.`);
+            } else if ((userNumber === systemNumber) || (userNumber === systemNumber - 1) || (userNumber === systemNumber + 1)) {
+                prize = 1;
+                console.log(`  You were close, keep the motivating prize: +1 point.`)
+            } else {
+                prize = 0;
+                console.log(`  This time you didn't guess, try again.`);
+            }
+            userPoints += prize;
+
+            console.log(`You have ${userPoints} points.`);
         } else {
-            prize = 0;
-            console.log(`This time you didn't guess, try again.`);
+            console.log("Please enter a number between 1 and 6!");
         }
-        userPoints += prize;
-
-        console.log(`You have ${userPoints} points.`);
 
         if (answer) {
             loop();
@@ -94,9 +56,9 @@ function loop() {
     });
 };
 
-loop();
-
 readline.on('SIGINT', () => {
     console.log(`\nYou finished the game with a score of: ${userPoints} points.`);
     readline.close();
 });
+
+loop();
