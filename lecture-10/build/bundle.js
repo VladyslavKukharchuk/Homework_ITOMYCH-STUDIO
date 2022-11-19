dc0b4683e42672748d2bb8dd4be91852d857c688
Vladyslav Kukharchuk <vladyslavziuziun@gmail.com>
2022-11-20 00:40:23 +0200

    Finished work on task 1

--- lecture-10/build/bundle.js
+++ lecture-10/build/bundle.js
@@ -7,3 +7,59 @@ console.log(isPointBelongsTriangle({ x: 3, y: 5 }, { x: 2, y: 5 }, { x: 5, y: 10
 console.log(isPointBelongsTriangle({ x: 4, y: 10 }, { x: 2, y: 5 }, { x: 5, y: 10 }, { x: 10, y: 5 }));
 console.log(isPointBelongsTriangle({ x: 6, y: 10 }, { x: 2, y: 5 }, { x: 5, y: 10 }, { x: 10, y: 5 }));
 console.log(isPointBelongsTriangle({ x: 10, y: 5 }, { x: 2, y: 5 }, { x: 5, y: 10 }, { x: 10, y: 5 }));
+console.log(isPointBelongsTriangle({ x: -11, y: -6 }, { x: -15, y: -5 }, { x: -10, y: -5 }, { x: -10, y: -10 }));
+console.log(isPointBelongsTriangle({ x: 8, y: 8 }, { x: -10, y: 10 }, { x: -5, y: 10 }, { x: -10, y: 5 }));
+console.log(isPointBelongsTriangle({ x: -2, y: 2 }, { x: -5, y: -5 }, { x: -5, y: 5 }, { x: 5, y: 5 }));
+let isCompleted = false;
+let decimal = 6;
+let integer = 7.10;
+let hex = 0xf00d;
+let binary = 0b1010;
+let octal = 0o744;
+let name1 = 'Yauhen';
+const u = undefined;
+const n = null;
+const greetUser = () => {
+    alert('Hello, nice to see you!');
+};
+let list1 = [1, 2, 3];
+let list2 = [1, 2, 3];
+let x1;
+x1 = ['goodboy', 42];
+let x2 = ['goodboy', 42];
+let y = ['goodboy', 42];
+let z = ['goodboy', 42];
+let notSure = false;
+var Directions;
+(function (Directions) {
+    Directions[Directions["Up"] = 0] = "Up";
+    Directions[Directions["Down"] = 1] = "Down";
+    Directions[Directions["Left"] = 6] = "Left";
+    Directions[Directions["Right"] = 7] = "Right";
+})(Directions || (Directions = {}));
+const msg = 'hello';
+const error = (msg) => {
+    throw new Error(msg);
+};
+let id1;
+let id2;
+let user1 = {
+    name: `Yauhen`,
+    age: 30
+};
+let user2 = {
+    name: `Yauhen`,
+    age: 30
+};
+const createPassword1 = (name, age) => `${name}${age}`;
+const createPassword2 = (age = 20) => `${age}`;
+const createPassword3 = (name, age) => `${name}${age}`;
+const createSkills = (name, ...skills) => `${name}, my skills are ${skills.join()}`;
+class User {
+    constructor(name, age, nickName) {
+        this.name = name;
+        this.age = age;
+        this.nickName = nickName;
+    }
+}
+const yauhen = new User('Yauhen', 22, 'webDev');
--- lecture-10/task1.ts
+++ lecture-10/task1.ts
@@ -1,2 +1,91 @@
 // Настроїти typescript для проекту
 // Використовувати ‘строгу’ типізацію
+
+
+//Приклад використання типів даних і строгої типізації в TypeScript:
+
+//boolean
+let isCompleted: boolean = false;
+//number
+let decimal: number = 6;
+let integer: number = 7.10;
+let hex: number = 0xf00d;
+let binary: number = 0b1010;
+let octal: number = 0o744;
+//string
+let name1: string = 'Yauhen';
+//undefined 
+const u: undefined = undefined;
+//null
+const n: null = null;
+//void
+const greetUser = (): void => {
+    alert('Hello, nice to see you!')
+}
+//array
+let list1: number[] = [1, 2, 3];
+let list2: Array<number> = [1, 2, 3];
+//tuple
+let x1: [string, number];
+x1 = ['goodboy', 42];
+//альтернативний запис
+let x2: [string, number] = ['goodboy', 42];
+//any
+let y: [any, any] = ['goodboy', 42];
+let z: Array<any> = ['goodboy', 42];
+let notSure: any = false;
+//enum
+enum Directions {
+    Up,       //0
+    Down,     //1
+    Left = 6, //6
+    Right     //7
+}
+//never
+// Функція повертає помилку
+const msg = 'hello';
+const error = (msg: string): never => {
+    throw new Error(msg);
+}
+
+//якщо змінна може мати декілька типів то оголошуємо через |
+let id1: number | string;
+//type - дозволяє створювати кастомні типи
+type Name = string;
+let id2: Name; //тепер змінна буде типу Name
+//object
+//звичайне оголошення
+let user1 = {
+    name: `Yauhen`,
+    age: 30
+}
+//вимога до типів
+let user2: { name: string, age: number } = {
+    name: `Yauhen`,
+    age: 30
+}
+//function
+//Оголошення функції з вказанням типу вхідних аргументів
+const createPassword1 = (name: string, age: number) => `${name}${age}`;
+//Вказання декількох типів для аргументів і дефолтного значення:
+const createPassword2 = (age: number | string = 20) => `${age}`;
+//Необов'язковий аргумент для функції:
+const createPassword3 = (name: string, age?: number) => `${name}${age}`;
+//Тип даних для Rest оператора:
+const createSkills = (name: string, ...skills: Array<string>) => `${name}, my skills are ${skills.join()}`;
+
+//class
+class User {
+
+    name: string;
+    age: number;
+    nickName: string;
+
+    constructor(name: string, age: number, nickName: string) {
+        this.name = name;
+        this.age = age;
+        this.nickName = nickName;
+    }
+}
+
+const yauhen = new User('Yauhen', 22, 'webDev');
