90bf09bb04d80defec44a7b3c0015189bad60139
Vladyslav Kukharchuk <vladyslavziuziun@gmail.com>
2022-11-21 17:46:13 +0200

    In process

--- lecture-8/Task2.js
+++ lecture-8/Task2.js
@@ -5,6 +5,210 @@
 // 4. Покупка товару
 // Реалізуйте 2 способами через callback і через проміси.
 
+// const users = [{
+//         name: "Roman",
+//         products: []
+//     },
+//     {
+//         name: "Vlad",
+//         products: []
+//     },
+//     {
+//         name: "Nazar",
+//         products: []
+//     }
+// ];
+
+// let currentUser = {};
+
+// function renderUsers() {
+//     console.log(`Registered users: `);
+//     users.map(user => console.log(`  ${user.name}`));
+// }
+
+// function renderUserInfo() {
+//     console.log(`name: ${currentUser.name}.`);
+//     getUserProducts();
+// }
+
+// function renderBusket() {
+//     if (basket.length > 0) {
+//         console.log(`Now in busket: `);
+//         basket.map(product => console.log(`   ${product.title} `));
+//         console.log(`Total: ${getTotal(basket)}.`);
+//     } else {
+//         console.log(`Busket is empty now.`);
+//     }
+// }
+
+// function renderProducts() {
+//     console.log(`Products that now in stock: `);
+//     products.map(product => console.log(`  ${product.title}, ціна: ${product.price}`));
+// }
+
+// function getUserProducts() {
+//     if (currentUser.products.length > 0) {
+//         console.log(`You bought before: `);
+//         currentUser.products.map(product => console.log(`   ${product.title}`));
+//         console.log(`Total: ${getTotal(currentUser.products)}.`);
+//     } else {
+//         console.log("You haven`t bought any products yet.");
+//     }
+// }
+
+// function autorization(name) {
+//     users.forEach((user, i, arr) => {
+//         if (user.name == name) {
+//             currentUser = user;
+//         }
+//     });
+
+//     if (!objectIsEmpty(currentUser)) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+
+// let basket = [];
+
+
+
+// function putInBasket(answer) {
+//     let answerArr = answer.split(' '),
+//         product = answerArr[1]
+
+//     products.forEach((item, i, arr) => {
+//         if (item.title === product) {
+//             basket.push(item)
+//         }
+//     });
+// }
+
+// function cleanBucket() {
+//     basket = [];
+// }
+
+// function buyProducts() {
+//     basket.forEach((item, i, arr) => {
+//         currentUser.products.push(item);
+//     });
+//     cleanBucket();
+// };
+
+// function getTotal(arr) {
+//     let total = 0;
+//     arr.forEach((product, i, arr) => {
+//         total += product.price;
+//     })
+//     return total;
+// }
+
+
+// const products = [{
+//         title: "Apples",
+//         price: 75
+//     },
+//     {
+//         title: "Beer",
+//         price: 50
+//     },
+//     {
+//         title: "Cookies",
+//         price: 100
+//     },
+//     {
+//         title: "Oranges",
+//         price: 125
+//     }
+// ];
+
+
+// function exit() {
+//     cleanBucket();
+//     currentUser = {};
+//     readline.close();
+// }
+
+
+// function objectIsEmpty(object) {
+//     return JSON.stringify(object) === '{}';
+// }
+
+
+// readline = require("readline").createInterface({
+//     input: process.stdin,
+//     output: process.stdout,
+// });
+
+
+// function login() {
+//     readline.question("\n\nTo log in, write your name: ", (answer) => {
+
+//         if (autorization(answer) === true) {
+//             console.log(`\n\n                             Hi ${answer}!\n\n`);
+//             renderBusket();
+//             renderProducts();
+//             shopApp();
+//         } else {
+//             console.log(`\n\n               User with name "${answer}" does not exist!\n`);
+//             login()
+//         }
+//     });
+// };
+
+// function eventProcessing(callback, data) {
+//     callback(data);
+
+//     renderBusket();
+
+//     renderProducts();
+
+//     shopApp();
+// }
+
+
+// function shopApp() {
+//     readline.question("\nComands: \n     to buy product: add <name>\n     to show your data: show\n     to finish shopping: by\n     to exit from shop: exit\nEnter comand in CLI:  ", (answer) => {
+
+//         if (answer.indexOf('add') !== -1) {
+
+//             eventProcessing(putInBasket, answer);
+
+//         } else if (answer.indexOf('show') !== -1) {
+
+//             eventProcessing(renderUserInfo);
+
+//         } else if (answer.indexOf('by') !== -1) {
+
+//             eventProcessing(buyProducts);
+
+//         } else if (answer.indexOf('exit') !== -1) {
+
+//             eventProcessing(exit);
+
+//         } else {
+//             console.log("Enter the command correctly!!");
+//             shopApp();
+//         }
+//     });
+// };
+
+
+// readline.on('SIGINT', () => {
+//     console.log(`\nThank you for coming!`);
+//     readline.close();
+// });
+
+
+
+// console.log("\n\n\n----------------------Welcome to our store!!----------------------\n\n                     Sign in to your account.\n\n\n");
+
+// renderUsers();
+
+// login();
+
 
 const users = [{
         name: "Roman",
@@ -20,17 +224,31 @@ const users = [{
     }
 ];
 
+let currentUser = {};
+
 function renderUsers() {
     console.log(`Registered users: `);
     users.map(user => console.log(`  ${user.name}`));
 }
 
-
-let currentUser = {};
-
 function renderUserInfo() {
     console.log(`name: ${currentUser.name}.`);
     getUserProducts();
+}
+
+function renderBusket() {
+    if (basket.length > 0) {
+        console.log(`Now in busket: `);
+        basket.map(product => console.log(`   ${product.title} `));
+        console.log(`Total: ${getTotal(basket)}.`);
+    } else {
+        console.log(`Busket is empty now.`);
+    }
+}
+
+function renderProducts() {
+    console.log(`Products that now in stock: `);
+    products.map(product => console.log(`  ${product.title}, ціна: ${product.price}`));
 }
 
 function getUserProducts() {
@@ -60,17 +278,12 @@ function autorization(name) {
 
 let basket = [];
 
-function renderBusket() {
-    if (basket.length > 0) {
-        console.log(`Now in busket: `);
-        basket.map(product => console.log(`   ${product.title} `));
-        console.log(`Total: ${getTotal(basket)}.`);
-    } else {
-        console.log(`Busket is empty now.`);
-    }
-}
 
-function putInBasket(product) {
+
+function putInBasket(answer) {
+    let answerArr = answer.split(' '),
+        product = answerArr[1]
+
     products.forEach((item, i, arr) => {
         if (item.title === product) {
             basket.push(item)
@@ -86,6 +299,7 @@ function buyProducts() {
     basket.forEach((item, i, arr) => {
         currentUser.products.push(item);
     });
+    cleanBucket();
 };
 
 function getTotal(arr) {
@@ -115,15 +329,11 @@ const products = [{
     }
 ];
 
-function renderProducts() {
-    console.log(`Products that now in stock: `);
-    products.map(product => console.log(`  ${product.title}, ціна: ${product.price}`));
-}
-
 
 function exit() {
     cleanBucket();
     currentUser = {};
+    readline.close();
 }
 
 
@@ -138,79 +348,92 @@ readline = require("readline").createInterface({
 });
 
 
-function login() {
+let login = new Promise((resolve, reject) => {
+    console.log("\n\n\n----------------------Welcome to our store!!----------------------\n\n                     Sign in to your account.\n\n\n");
+
+    renderUsers();
+
     readline.question("\n\nTo log in, write your name: ", (answer) => {
 
         if (autorization(answer) === true) {
             console.log(`\n\n                             Hi ${answer}!\n\n`);
             renderBusket();
             renderProducts();
-            shopApp();
+            // shopApp();
+            resolve(answer);
         } else {
             console.log(`\n\n               User with name "${answer}" does not exist!\n`);
-            // readline.close();
             login()
         }
-    });
-};
+    })
+});
+
+function eventProcessing(callback, data) {
+    callback(data);
+
+    renderBusket();
+
+    renderProducts();
+
+    shopApp();
+}
 
 
-function shopApp() {
+// function shopApp() {
+//     readline.question("\nComands: \n     to buy product: add <name>\n     to show your data: show\n     to finish shopping: by\n     to exit from shop: exit\nEnter comand in CLI:  ", (answer) => {
+
+//         if (answer.indexOf('add') !== -1) {
+
+//             eventProcessing(putInBasket, answer);
+
+//         } else if (answer.indexOf('show') !== -1) {
+
+//             eventProcessing(renderUserInfo);
+
+//         } else if (answer.indexOf('by') !== -1) {
+
+//             eventProcessing(buyProducts);
+
+//         } else if (answer.indexOf('exit') !== -1) {
+
+//             eventProcessing(exit);
+
+//         } else {
+//             console.log("Enter the command correctly!!");
+//             shopApp();
+//         }
+//     });
+// };
+
+let userAnswer = new Promise((resolve, reject) => {
     readline.question("\nComands: \n     to buy product: add <name>\n     to show your data: show\n     to finish shopping: by\n     to exit from shop: exit\nEnter comand in CLI:  ", (answer) => {
-        console.log("--------------------------RESULT--------------------------");
+        resolve(answer);
+    })
+});
+// userAnswer.then((answer) => {
+//     console.log(answer);
+//     if (answer.indexOf('add') !== -1) {
 
-        if (answer.indexOf('add') !== -1) {
-            let answerArr = answer.split(' ');
-            putInBasket(answerArr[1]);
+//         eventProcessing(putInBasket, answer);
 
-            renderBusket();
+//     } else if (answer.indexOf('show') !== -1) {
 
-            console.log("---------------------------INFO---------------------------");
+//         eventProcessing(renderUserInfo);
 
-            renderProducts();
+//     } else if (answer.indexOf('by') !== -1) {
 
-            console.log("----------------------------------------------------------");
-            shopApp();
-        } else if (answer.indexOf('show') !== -1) {
+//         eventProcessing(buyProducts);
 
-            renderUserInfo();
+//     } else if (answer.indexOf('exit') !== -1) {
 
-            console.log("---------------------------INFO---------------------------");
+//         eventProcessing(exit);
 
-            renderBusket();
-            renderProducts();
+//     } else {
+//         console.log("Enter the command correctly!!");
+//         shopApp();
+//     }
+// })
 
-            console.log("----------------------------------------------------------");
-            shopApp();
-        } else if (answer.indexOf('by') !== -1) {
-            buyProducts();
-            cleanBucket();
-
-            console.log(`\n\n\n                Thank you for shopping!\n\n\n `);
-
-            console.log("------------------------UPDATE-INFO------------------------");
-
-            renderUserInfo();
-            console.log("----------------------------------------------------------");
-            renderBusket();
-
-            console.log("---------------------------INFO---------------------------");
-
-            renderProducts();
-
-            console.log("----------------------------------------------------------");
-            shopApp();
-        } else if (answer.indexOf('exit') !== -1) {
-            console.log(`\n\n\n                Thank you for coming!          \n\n\n`);
-            exit();
-            console.log("----------------------------------------------------------");
-            readline.close();
-        } else {
-            console.log("Enter the command correctly!!");
-            shopApp();
-        }
-    });
-};
 
 
 readline.on('SIGINT', () => {
@@ -220,10 +443,52 @@ readline.on('SIGINT', () => {
 
 
 
-console.log("\n\n\n----------------------Welcome to our store!!----------------------\n\n                     Sign in to your account.\n\n\n");
+// login.then(() => {
+//     userAnswer.then((answer) => {
+//         console.log(answer);
+//         if (answer.indexOf('add') !== -1) {
 
-renderUsers();
+//             eventProcessing(putInBasket, answer);
 
-login();
+//         } else if (answer.indexOf('show') !== -1) {
 
-// shopApp();
+//             eventProcessing(renderUserInfo);
+
+//         } else if (answer.indexOf('by') !== -1) {
+
+//             eventProcessing(buyProducts);
+
+//         } else if (answer.indexOf('exit') !== -1) {
+
+//             eventProcessing(exit);
+
+//         } else {
+//             console.log("Enter the command correctly!!");
+//             shopApp();
+//         }
+//     })
+// })
+
+userAnswer.then((answer) => {
+    console.log(answer);
+    if (answer.indexOf('add') !== -1) {
+
+        eventProcessing(putInBasket, answer);
+
+    } else if (answer.indexOf('show') !== -1) {
+
+        eventProcessing(renderUserInfo);
+
+    } else if (answer.indexOf('by') !== -1) {
+
+        eventProcessing(buyProducts);
+
+    } else if (answer.indexOf('exit') !== -1) {
+
+        eventProcessing(exit);
+
+    } else {
+        console.log("Enter the command correctly!!");
+        shopApp();
+    }
+})
