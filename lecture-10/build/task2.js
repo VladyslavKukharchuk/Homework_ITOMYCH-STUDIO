8c2fb2b78b01d10cd3a971ba1c4a1f95eee981a3
Vladyslav Kukharchuk <vladyslavziuziun@gmail.com>
2022-11-25 17:05:21 +0200

    works only with positive numbers

--- lecture-10/build/task2.js
+++ lecture-10/build/task2.js
@@ -8,10 +8,10 @@
 function multiplyPolynomials(p1, p2) {
     let p1Arr = p1.split(/[+,-]/, 2);
     let p2Arr = p2.split(/[+,-]/, 2);
-    console.log(p1Arr);
-    console.log(p2Arr);
-    console.log(+p1Arr[0]);
-    console.log(+p1Arr[1]);
+    // console.log(p1Arr);
+    // console.log(p2Arr);
+    // console.log(+p1Arr[0]);
+    // console.log(+p1Arr[1]);
     function parse(x) {
         if (isNaN(+x)) {
             if (x.includes("^")) {
@@ -24,48 +24,66 @@ function multiplyPolynomials(p1, p2) {
             return +x;
         }
     }
-    console.log(`p1Arr[0]`);
-    console.log(parse(p1Arr[0]));
-    console.log(`p1Arr[1]`);
-    console.log(parse(p1Arr[1]));
-    let x1Arr = [1, p1Arr[0], 1];
-    let x2Arr = [1, p2Arr[0], 1];
-    if (p1Arr[0].includes("^")) {
-        let x1 = p1Arr[0].split("^", 2);
-        x1Arr = [1, x1[0], parseInt(x1[1])];
-    }
-    if (p2Arr[0].includes("^")) {
-        let x2 = p2Arr[0].split("^", 2);
-        x2Arr = [1, x2[0], parseInt(x2[1])];
-    }
-    let a = x1Arr, b = parseInt(p1Arr[1]), c = x2Arr, d = parseInt(p2Arr[1]);
-    console.log(`a: ${a}`);
-    console.log(`b: ${b}`);
-    console.log(`c: ${c}`);
-    console.log(`d: ${d}`);
+    // console.log(`p1Arr[0]`)
+    // console.log(parse(p1Arr[0]))
+    // console.log(`p1Arr[1]`)
+    // console.log(parse(p1Arr[1]))
+    // let x1Arr = [1, p1Arr[0], 1];
+    // let x2Arr = [1, p2Arr[0], 1];
+    // if (p1Arr[0].includes("^")) {
+    //     let x1 = p1Arr[0].split("^", 2);
+    //     x1Arr = [1, x1[0], parseInt(x1[1])];
+    // }
+    // if (p2Arr[0].includes("^")) {
+    //     let x2 = p2Arr[0].split("^", 2);
+    //     x2Arr = [1, x2[0], parseInt(x2[1])];
+    // }
+    let a = parse(p1Arr[0]), b = parse(p1Arr[1]), c = parse(p2Arr[0]), d = parse(p2Arr[1]);
+    // console.log(`a: ${a}`);
+    // console.log(`b: ${b}`);
+    // console.log(`c: ${c}`);
+    // console.log(`d: ${d}`);
     function multiply(x, y) {
         if (Array.isArray(x) && (typeof (y) === 'number')) {
-            console.log("Arr * number");
             return [x[0] * y, x[1], x[2]];
         }
         else if ((typeof (x) === 'number') && Array.isArray(y)) {
-            console.log("number * Arr");
             return [y[0] * x, y[1], y[2]];
         }
         else if ((typeof (x) === 'number') && (typeof (y) === 'number')) {
-            console.log("number * number");
             return x * y;
         }
         else if (Array.isArray(x) && Array.isArray(y)) {
-            console.log("Arr * Arr");
             return [x[0] * y[0], x[1], x[2] + y[2]];
         }
     }
-    console.log(multiply(a, c));
-    console.log(multiply(a, d));
-    console.log(multiply(b, c));
-    console.log(multiply(b, d));
+    // console.log(multiply(a, c));
+    // console.log(multiply(a, d));
+    // console.log(multiply(b, c));
+    // console.log(multiply(b, d));
+    function format(x) {
+        if (isNaN(+x)) {
+            if ((x[0] !== 1) && (x[2] !== 1)) {
+                return `${x[0]}${x[1]}^${x[2]}`;
+            }
+            else if ((x[0] === 1) && (x[2] !== 1)) {
+                return `${x[1]}^${x[2]}`;
+            }
+            else if ((x[0] !== 1) && (x[2] === 1)) {
+                return `${x[0]}${x[1]}`;
+            }
+            else if ((x[0] === 1) && (x[2] === 1)) {
+                return `${x[1]}`;
+            }
+        }
+        return x.toString();
+    }
+    // console.log(format(multiply(a, c)));
+    // console.log(format(multiply(a, d)));
+    // console.log(format(multiply(b, c)));
+    // console.log(format(multiply(b, d)));
     // let result = a * c + a * d + b * c + b * d;
-    return "1+x^2+x^3+x^5";
+    return `${format(multiply(a, c))}+${format(multiply(a, d))}+${format(multiply(b, c))}+${format(multiply(b, d))}`;
 }
-multiplyPolynomials("x^3+1", "x^2+1");
+console.log(multiplyPolynomials("x^3+1", "x^2+1"));
+console.log(multiplyPolynomials("x+1", "x-1"));
